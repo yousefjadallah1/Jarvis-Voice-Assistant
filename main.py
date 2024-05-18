6898dfea502a258af8e904ae7a31c5a966d15fc6
Yousef Jadallah <yousef.jadallah@outlook.com>
2024-05-18 02:16:05 +0200

    update

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import datetime
 import random
-
 import wikipedia
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import pyjokes
 
+
+
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('rate', 150)
+engine.setProperty('rate', 180)
 engine.setProperty('voice', voices[2].id)
 
+
+
 start_sentence = [
-    "Hi, I am Jarvis",
-    "Hello there, I am Jarvis, at your service.",
-    "Hey there, I am Jarvis, at your service.",
-    "Hello, I am Jarvis, designed to assist you.",
-    "Hey, I am Jarvis, designed to assist you.",
-    "Hello sir, I am Jarvis",
-    "Hi, I am Jarvis, At your service",
-    "Hey, I am Jarvis"
+    "Hi, I am jarvis",
+    "Hello there, I am jarvis, at your service.",
+    "Hey there, I am jarvis, at your service.",
+    "Hello, I am jarvis, designed to assist you.",
+    "Hey, I am jarvis, designed to assist you.",
+    "Hello sir, I am jarvis",
+    "Hi, I am jarvis, At your service",
+    "Hey, I am jarvis"
 ]
 
 help_sentences = [
@@ -37,17 +40,17 @@ help_sentences = [
 ]
 
 greetings = [
-    "hi Jarvis",
-    "hello Jarvis",
+    "hi jarvis",
+    "hello jarvis",
     "Hi",
     "Hello",
     "hey there",
     "hi there",
-    "hey Jarvis",
-    "good morning Jarvis",
-    "good afternoon Jarvis",
-    "good evening Jarvis",
-    "Jarvis, greetings",
+    "hey Siri",
+    "good morning jarvis",
+    "good afternoon jarvis",
+    "good evening jarvis",
+    "jarvis, greetings",
 ]
 
 greeting_responses = [
@@ -61,8 +64,12 @@ greeting_responses = [
     "What can I do for you, sir?",
     "Hello! What's on your mind?"
 ]
-
+else_keywords = ["didn't get it", "again please", "What's that", "another time please"]
+stupid_keywords = ["haha think thats funny", "No you are", "crazy really", "Don't say that or I will block you", "I'm smarter than you if you now lol"]
 music_keywords = ['play', 'song', 'music', 'play for me']
+how_are_you_keywords = ["I'm great thanks for asking", "I'm good", "I'm fine thanks", "I am OK", "not my best day"]
+wikipedia_keywords = ['search' , 'who is', 'what is', 'wikipedia']
+
 
 start_sen = random.choice(start_sentence)
 engine.say(start_sen)
@@ -81,7 +88,7 @@ def talk(text):
 def take_command():
     try:
         with sr.Microphone() as source:
-            print('Listening to you Sir ...')
+            print('Listening to you ...')
             listener.adjust_for_ambient_noise(source)
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
@@ -104,14 +111,20 @@ def run_jarvis():
         response = random.choice(greeting_responses)
         talk(response)
 
+    elif 'how are you' in command:
+        talk(random.choice(how_are_you_keywords))
+
+    elif 'stupid' in command:
+        talk(random.choice(stupid_keywords))
+
         # check music commands
     elif any(keyword in command for keyword in music_keywords):
         music(command)
     #     check for time
     elif 'time' in command:
         time()
-    #     check for wikipedia
-    elif 'who is' in command or 'search about' in command:
+    #     check in wikipedia
+    elif any(keyword in command for keyword in wikipedia_keywords):
         informantion(command)
     #     check for jokes
     elif 'joke' in command:
@@ -122,8 +135,8 @@ def run_jarvis():
 
 
 def informantion(command):
-    something = command.replace('search about' or 'who is', '')
-    info = wikipedia.summary(something, 1)
+    something = command.replace('search about', '')
+    info = wikipedia.summary(something, 2)
     talk(info)
 
 def music(command):
